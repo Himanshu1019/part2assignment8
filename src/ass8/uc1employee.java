eebe4b25b81b39bb3477d6fc428474a35cc2ca24
classroom <himanshukanoje23@gmail.com>
2022-04-02 15:00:44 +0530

    my commit

--- src/ass8/uc1employee.java
+++ src/ass8/uc1employee.java
@@ -15,13 +15,16 @@ public class uc1employee {
 	}
 			class Calculate
 			{
+				
 				public static void m1()
 				{
+					int hour=20,wage=20,total;
 				int win =(int) Math.floor(Math.random()*10)%2;
 				if (win==1) 
 				{
 					System.out.println(" employee is present");
-					
+					total=hour*wage;
+					System.out.println(" the total wage for the employee todays is "+ total);
 				}
 				else if(win==0)
 				{
