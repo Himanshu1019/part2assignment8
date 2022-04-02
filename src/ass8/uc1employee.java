17233615894cb165351d289a37e2af48cb0bb354
classroom <himanshukanoje23@gmail.com>
2022-04-02 16:00:58 +0530

    my commit

--- src/ass8/uc1employee.java
+++ src/ass8/uc1employee.java
@@ -18,16 +18,19 @@ public class uc1employee {
 				
 				public static void m1()
 				{
-					int hour=160,wage,total;
+					int hour,wage,total=0,days,totalhours;
 				int win =(int) Math.floor(Math.random()*10)%2;
 				if (win==1) 
 				{
 					System.out.println(" employeer is present");
 					Scanner s = new Scanner(System.in);
-					System.out.println("enter the amount per hour for an employeer ");
+					System.out.println("enter the no of days employee is working in a month ");
+					 days= s.nextInt();
+					System.out.println("enter the amount per hour in a day ");
 					wage = s.nextInt();
+					totalhours=days*8;
+					total=totalhours*wage;
 					
-					total=hour*wage;
 					System.out.println(" the total wage for the employeer this month is  "+ total);
 				}
 				else if(win==0)
